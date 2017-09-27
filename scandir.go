9f2a68a66f745a43ae274f624597abec6e867a6d
Julien Marusi <julien.marusi@watchever.com>
2017-09-27 11:45:56 +0200

    correctif regexp

--- scandir.go
+++ scandir.go
@@ -137,7 +137,7 @@ func MakePrettyName(UglyName string) (map[string]string, string) {
 		return results, ""
 	}
 
-	regex = regexp.MustCompile(`(?iU)^(.+?)[. _\-\t][.( _\t](?:19\d{2}|20(?:0\d|1[0-9])).*[.](mkv|avi|mpe?g|mp4)$`)
+	regex = regexp.MustCompile(`(?iU)^(.+?)[.( _\t](?:19\d{2}|20(?:0\d|1[0-9])).*[.](mkv|avi|mpe?g|mp4)$`)
 	// regex := regexp.MustCompile(`(?iU)^(.+?)[.( \t]*((19\d{2}|20(?:0\d|1[0-9])).*|(?:(?=\d+p|bluray|brrip|webrip|hdlight|dvdrip|web-dl|hdrip)..*)?[.](mkv|avi|mpe?g|mp4)$)`)
 	infosBase := regex.FindStringSubmatch(UglyName)
 	regex = regexp.MustCompile(`(?iU)^(?:.+?)(19\d{2}|20(?:0\d|1[0-9]))(?:.+?)$`)
