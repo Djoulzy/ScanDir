8c51e8b62d0556cad02ecbd4d1526ead046986be
Julien Marusi <julien.marusi@watchever.com>
2017-09-28 15:33:23 +0200

    Reorg pour le tri

--- scandir.go
+++ scandir.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"github.com/Djoulzy/MovieDB"
+	"github.com/Djoulzy/ScanDir/stripchar"
 	"github.com/Djoulzy/Tools/clog"
 	tmdb "github.com/ryanbradynd05/go-tmdb"
 )
@@ -121,7 +122,7 @@ func fullList(root string) {
 	fmt.Printf("filepath.Walk() returned %v\n", err)
 }
 
-func MakePrettyName(UglyName string) (map[string]string, string) {
+func isPrettyName(UglyName string) (map[string]string, bool) {
 	regex := regexp.MustCompile(`(?iU)^(.+?)_\((19\d{2}|20(?:0\d|1[0-9]))\)_(multi|vf(?:\w*)|(?:\w*)french)?_(\d+p)?_(bluray|brrip|webrip|hdlight|dvdrip|web-dl|hdrip)?_\[(\d+)?\]\.(mkv|avi|mpe?g|mp4)$`)
 	globalRule := regex.FindStringSubmatch(UglyName)
 
@@ -134,116 +135,128 @@ func MakePrettyName(UglyName string) (map[string]string, string) {
 		results["origine"] = globalRule[5]
 		results["tmdbid"] = globalRule[6]
 		results["ext"] = globalRule[7]
-		return results, ""
-	}
-
-	regex = regexp.MustCompile(`(?iU)^(.+?)[.( _\t](?:19\d{2}|20(?:0\d|1[0-9])).*[.](mkv|avi|mpe?g|mp4)$`)
-	// regex := regexp.MustCompile(`(?iU)^(.+?)[.( \t]*((19\d{2}|20(?:0\d|1[0-9])).*|(?:(?=\d+p|bluray|brrip|webrip|hdlight|dvdrip|web-dl|hdrip)..*)?[.](mkv|avi|mpe?g|mp4)$)`)
-	infosBase := regex.FindStringSubmatch(UglyName)
-	regex = regexp.MustCompile(`(?iU)^(?:.+?)(19\d{2}|20(?:0\d|1[0-9]))(?:.+?)$`)
-	year := regex.FindStringSubmatch(UglyName)
-	regex = regexp.MustCompile(`(?iU)^(?:.+?)(bluray|brrip|webrip|hdlight|dvdrip|web-dl|hdrip)(?:.+?)$`)
-	origine := regex.FindStringSubmatch(UglyName)
-	regex = regexp.MustCompile(`(?iU)^(?:.+?)(?:[^\d](\d+p)[^p])(?:.*?)$`)
-	qualite := regex.FindStringSubmatch(UglyName)
-	regex = regexp.MustCompile(`(?iU)^(?:.+?)(multi|vf(?:\w*)|(?:\w*)french)(?:.+?)$`)
-	langue := regex.FindStringSubmatch(UglyName)
-
-	if len(infosBase) > 2 {
-		results["titre"] = infosBase[1]
-		results["ext"] = infosBase[2]
+		return results, true
 	} else {
-		infosBase := strings.Split(UglyName, ".")
-		results["titre"] = infosBase[0]
-		results["ext"] = infosBase[1]
-	}
-	if len(year) == 2 {
-		results["year"] = year[1]
-	}
-	if len(origine) == 2 {
-		results["origine"] = origine[1]
-	}
-	if len(qualite) == 2 {
-		results["qualite"] = qualite[1]
-	}
-	if len(langue) == 2 {
-		results["langue"] = langue[1]
-	}
+		regex = regexp.MustCompile(`(?iU)^(.+)[.( _\t]+(?:19\d{2}|20(?:0\d|1[0-9])).*[.](mkv|avi|mpe?g|mp4)$`)
+		// regex := regexp.MustCompile(`(?iU)^(.+?)[.( \t]*((19\d{2}|20(?:0\d|1[0-9])).*|(?:(?=\d+p|bluray|brrip|webrip|hdlight|dvdrip|web-dl|hdrip)..*)?[.](mkv|avi|mpe?g|mp4)$)`)
+		infosBase := regex.FindStringSubmatch(UglyName)
+		regex = regexp.MustCompile(`(?iU)^(?:.+?)(19\d{2}|20(?:0\d|1[0-9]))(?:.+?)$`)
+		year := regex.FindStringSubmatch(UglyName)
+		regex = regexp.MustCompile(`(?iU)^(?:.+?)(bluray|brrip|webrip|hdlight|dvdrip|web-dl|hdrip)(?:.+?)$`)
+		origine := regex.FindStringSubmatch(UglyName)
+		regex = regexp.MustCompile(`(?iU)^(?:.+?)(?:[^\d](\d+p)[^p])(?:.*?)$`)
+		qualite := regex.FindStringSubmatch(UglyName)
+		regex = regexp.MustCompile(`(?iU)^(?:.+?)(multi|vf(?:\w*)|(?:\w*)french)(?:.+?)$`)
+		langue := regex.FindStringSubmatch(UglyName)
 
-	NBsep := strings.Count(UglyName, " ")
-	sep := " "
-	tmp := strings.Count(UglyName, ".")
-	if tmp > NBsep {
-		NBsep = tmp
-		sep = "."
-	}
-	tmp = strings.Count(UglyName, "_")
-	if tmp > NBsep {
-		NBsep = tmp
-		sep = "_"
-	}
-	tmp = strings.Count(UglyName, "-")
-	if tmp > NBsep {
-		NBsep = tmp
-		sep = "-"
-	}
-	if sep != " " {
-		results["titre"] = strings.Replace(results["titre"], sep, " ", -1)
-	}
-	results["titre"] = strings.Trim(results["titre"], " ")
+		if len(infosBase) > 2 {
+			results["titre"] = infosBase[1]
+			results["ext"] = infosBase[2]
+		} else {
+			infosBase := strings.Split(UglyName, ".")
+			results["titre"] = infosBase[0]
+			results["ext"] = infosBase[1]
+		}
+		if len(year) == 2 {
+			results["year"] = year[1]
+		}
+		if len(origine) == 2 {
+			results["origine"] = origine[1]
+		}
+		if len(qualite) == 2 {
+			results["qualite"] = qualite[1]
+		}
+		if len(langue) == 2 {
+			results["langue"] = langue[1]
+		}
 
-	id, err := myDB.GetMovieID(strings.ToLower(results["titre"]), results["year"])
-	if err != nil {
-		return results, ""
+		NBsep := strings.Count(UglyName, " ")
+		sep := " "
+		tmp := strings.Count(UglyName, ".")
+		if tmp > NBsep {
+			NBsep = tmp
+			sep = "."
+		}
+		tmp = strings.Count(UglyName, "_")
+		if tmp > NBsep {
+			NBsep = tmp
+			sep = "_"
+		}
+		tmp = strings.Count(UglyName, "-")
+		if tmp > NBsep {
+			NBsep = tmp
+			sep = "-"
+		}
+		if sep != " " {
+			results["titre"] = strings.Replace(results["titre"], sep, " ", -1)
+		}
+		results["titre"] = strings.Trim(results["titre"], " ")
+		return results, false
 	}
-	raw, err := myDB.GetMovieInfos(id)
-	var dbMovieInfos = &tmdb.Movie{}
-	err = json.Unmarshal(raw, dbMovieInfos)
+}
+
+func renameFile(path string, from string, with map[string]string) (os.FileInfo, error) {
+	titre := stripchar.StripCtlAndExtFromUnicode(with["titre"])
+	id, err := myDB.GetMovieID(string(titre), with["year"])
 	if err == nil {
-		results["titre"] = dbMovieInfos.Title
-		reldate := strings.Split(dbMovieInfos.ReleaseDate, "-")
-		results["year"] = reldate[0]
+		raw, err := myDB.GetMovieInfos(id)
+		var dbMovieInfos = &tmdb.Movie{}
+		err = json.Unmarshal(raw, dbMovieInfos)
+		if err == nil {
+			with["titre"] = dbMovieInfos.Title
+			with["year"] = strings.Split(dbMovieInfos.ReleaseDate, "-")[0]
+		}
+		newFileName := fmt.Sprintf("%s_(%s)_%s_%s_%s_[%s].%s", with["titre"], with["year"], with["langue"], with["qualite"], with["origine"], id, with["ext"])
+		clog.Info("scandir", "renameFile", "FROM: %s TO: %s", from, newFileName)
+		destFile := fmt.Sprintf("%s%s", path, newFileName)
+		os.Rename(fmt.Sprintf("%s%s", path, from), destFile)
+		return os.Stat(destFile)
+	} else {
+		return nil, err
 	}
-	return results, id
 }
 
-func renameFile(path string, from string, id string, with map[string]string) {
-	newFileName := fmt.Sprintf("%s_(%s)_%s_%s_%s_[%s].%s", with["titre"], with["year"], with["langue"], with["qualite"], with["origine"], id, with["ext"])
-	clog.Info("scandir", "renameFile", "FROM: %s TO: %s", from, newFileName)
-	os.Rename(fmt.Sprintf("%s%s", path, from), fmt.Sprintf("%s%s", path, newFileName))
-}
-
-func simpleList(prefix string, root string, base string, pagenum int, nbperpage int) (items, pagination) {
-	var stop int
-	var totalFiles int
-
-	theDir := fmt.Sprintf("%s%s", prefix, root)
+func makeCorrectFileList(theDir string) ([]os.FileInfo, error) {
 	files, err := ioutil.ReadDir(theDir)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	stat, _ := os.Stat(theDir)
-	totalFiles = len(files)
 
-	var zeFilez items
-
-	if nbperpage == 0 {
-		nbperpage = totalFiles
-	}
-	index := nbperpage * (pagenum - 1)
-	if index+nbperpage > totalFiles {
-		stop = totalFiles
-	} else {
-		stop = index + nbperpage
-	}
-	for index < stop {
-		// for _, f := range files {
-		f := files[index]
-		index++
+	var tmp []os.FileInfo
+	for _, f := range files {
 		fileName := f.Name()
 		if filepath.HasPrefix(fileName, ".") || filepath.HasPrefix(fileName, "@") || filepath.HasPrefix(fileName, "_") || filepath.HasPrefix(fileName, "thumbs") {
 			continue
 		}
+
+		if !f.IsDir() {
+			newInfos := f
+			infos, ok := isPrettyName(fileName)
+			if !ok {
+				newf, err := renameFile(theDir, fileName, infos)
+				if err == nil {
+					newInfos = newf
+				}
+			}
+			tmp = append(tmp, newInfos)
+		} else {
+			tmp = append(tmp, f)
+		}
+	}
+	return tmp, nil
+}
+
+func simpleList(prefix string, root string, base string) items {
+	theDir := fmt.Sprintf("%s%s/", prefix, root)
+	files, err := makeCorrectFileList(theDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	stat, _ := os.Stat(theDir)
+
+	var zeFilez items
+	for _, f := range files {
+		fileName := f.Name()
 
 		stat, _ = os.Stat(fmt.Sprintf("%s/%s", theDir, fileName))
 		modTime := stat.ModTime()
@@ -255,11 +268,7 @@ func simpleList(prefix string, root string, base string, pagenum int, nbperpage 
 			tmp.NBItems = len(tmpfiles)
 			// tmp.Items = simpleList(prefix, fmt.Sprintf("%s/%s", root, f.Name()), fmt.Sprintf("%s/%s", base, f.Name()))
 		} else {
-			infos, id := MakePrettyName(fileName)
-			if id != "" {
-				infos["tmdbid"] = id
-				renameFile(fmt.Sprintf("%s%s/", prefix, root), fileName, id, infos)
-			}
+			infos, _ := isPrettyName(fileName)
 			tmp.TMDBID = infos["tmdbid"]
 			tmp.Name = infos["titre"]
 			tmp.Type = "file"
@@ -278,44 +287,49 @@ func simpleList(prefix string, root string, base string, pagenum int, nbperpage 
 		tmp.ModTime = modTime
 		zeFilez = append(zeFilez, tmp)
 	}
-
-	tmp := pagination{
-		totalFiles: totalFiles,
-		totalPages: totalFiles / nbperpage,
-		actualPage: pagenum,
-		nbPerPage:  nbperpage,
-	}
-	return zeFilez, tmp
+	return zeFilez
 }
 
 func Start(appConf DataSource, TMDB *MovieDB.MDB, root string, orderby string, asc bool, pagenum int, nbperpage int) []byte {
 	globalConf = appConf
 	myDB = TMDB
 	base := filepath.Base(root)
+
+	clog.Info("ScanDir", "Start", "Prefix: %s, Dir: %s, OrderBy: %s (ASC:%v) Page: %d, NbPP: %d", appConf.GetPrefixDir(), root, orderby, asc, pagenum, nbperpage)
+	list := simpleList(appConf.GetPrefixDir(), root, base)
+
 	if pagenum == 0 {
 		pagenum = 1
 	}
-	list, total := simpleList(appConf.GetPrefixDir(), root, base, pagenum, nbperpage)
+	if nbperpage == 0 {
+		nbperpage = len(list)
+	}
 
-	clog.Info("ScanDir", "Start", "Prefix: %s, Dir: %s, OrderBy: %s (ASC:%v) Page: %d, NbPP: %d", appConf.GetPrefixDir(), root, orderby, asc, pagenum, nbperpage)
+	index := nbperpage * (pagenum - 1)
+	stop := index + nbperpage
+	if index+nbperpage > len(list) {
+		stop = len(list)
+	}
+
+	var finalList = list[index:stop]
 	switch orderby {
 	case "title":
 		if asc {
-			sort.Sort(ByTitle(list))
+			sort.Sort(ByTitle(finalList))
 		} else {
-			sort.Sort(sort.Reverse(ByTitle(list)))
+			sort.Sort(sort.Reverse(ByTitle(finalList)))
 		}
 	case "date":
 		if asc {
-			sort.Sort(ByDate(list))
+			sort.Sort(ByDate(finalList))
 		} else {
-			sort.Sort(sort.Reverse(ByDate(list)))
+			sort.Sort(sort.Reverse(ByDate(finalList)))
 		}
 	case "year":
 		if asc {
-			sort.Sort(ByYear(list))
+			sort.Sort(ByYear(finalList))
 		} else {
-			sort.Sort(sort.Reverse(ByYear(list)))
+			sort.Sort(sort.Reverse(ByYear(finalList)))
 		}
 	}
 
@@ -324,11 +338,11 @@ func Start(appConf DataSource, TMDB *MovieDB.MDB, root string, orderby string, a
 		Name:          base,
 		Path:          root,
 		Type:          "folder",
-		Items:         list,
-		NBItems:       total.totalFiles,
-		NBPages:       total.totalPages,
-		DisplayedPage: total.actualPage,
-		ItemsPerPage:  total.nbPerPage,
+		Items:         finalList,
+		NBItems:       len(list),
+		NBPages:       len(list) / nbperpage,
+		DisplayedPage: pagenum,
+		ItemsPerPage:  nbperpage,
 	}
 
 	// json, _ := json.MarshalIndent(rootFiles, "", "    ")
---
+++ stripchar/stripchar.go
@@ -0,0 +1,94 @@
+package stripchar
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"unicode/utf16"
+	"unicode/utf8"
+
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
+)
+
+func DecodeUTF16(b []byte) (string, error) {
+	if len(b)%2 != 0 {
+		return "", fmt.Errorf("Must have even length byte slice")
+	}
+
+	u16s := make([]uint16, 1)
+	ret := &bytes.Buffer{}
+	b8buf := make([]byte, 4)
+
+	lb := len(b)
+	for i := 0; i < lb; i += 2 {
+		u16s[0] = uint16(b[i]) + (uint16(b[i+1]) << 8)
+		r := utf16.Decode(u16s)
+		n := utf8.EncodeRune(b8buf, r[0])
+		ret.Write(b8buf[:n])
+	}
+	return ret.String(), nil
+}
+
+// two byte-oriented functions identical except for operator comparing c to 127.
+func stripCtlFromBytes(str string) string {
+	b := make([]byte, len(str))
+	var bl int
+	for i := 0; i < len(str); i++ {
+		c := str[i]
+		if c >= 32 && c != 127 {
+			b[bl] = c
+			bl++
+		}
+	}
+	return string(b[:bl])
+}
+
+func stripCtlAndExtFromBytes(str string) string {
+	b := make([]byte, len(str))
+	var bl int
+	for i := 0; i < len(str); i++ {
+		c := str[i]
+		if c >= 32 && c < 127 {
+			b[bl] = c
+			bl++
+		}
+	}
+	return string(b[:bl])
+}
+
+// two UTF-8 functions identical except for operator comparing c to 127
+func stripCtlFromUTF8(str string) string {
+	return strings.Map(func(r rune) rune {
+		if r >= 32 && r != 127 {
+			return r
+		}
+		return -1
+	}, str)
+}
+
+func stripCtlAndExtFromUTF8(str string) string {
+	return strings.Map(func(r rune) rune {
+		if r >= 32 && r < 127 {
+			return r
+		}
+		return -1
+	}, str)
+}
+
+// Advanced Unicode normalization and filtering,
+// see http://blog.golang.org/normalization and
+// http://godoc.org/golang.org/x/text/unicode/norm for more
+// details.
+func StripCtlAndExtFromUnicode(str string) string {
+	isOk := func(r rune) bool {
+		return r < 32 || r >= 127
+	}
+	// The isOk filter is such that there is no need to chain to norm.NFC
+	t := transform.Chain(norm.NFKD, transform.RemoveFunc(isOk))
+	// This Transformer could also trivially be applied as an io.Reader
+	// or io.Writer filter to automatically do such filtering when reading
+	// or writing data anywhere.
+	str, _, _ = transform.String(t, str)
+	return str
+}
